d204ad2ffaaa343ee5e7d4b7dbb028f556695eef
Chriss Quartz <chrissquartz@gmail.com>
2026-05-05 20:35:12 +1000

    fix

--- python_playwright/entrypoint.sh
+++ python_playwright/entrypoint.sh
@@ -11,7 +11,7 @@ python --version
 # Check if playwright module is installed
 if python -c "import importlib.util; exit(0 if importlib.util.find_spec('playwright') else 1)"; then
     echo "Playwright module detected. Installing browsers..."
-    python -m playwright install
+    python -m playwright install --with-deps
 else
     echo "Playwright module not found. Skipping browser installation."
 fi
